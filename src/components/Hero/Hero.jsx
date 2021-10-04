8256ee879009385dd50a2abc562b2f83e18ca937
Macarena Zalazar <82382979+MacarenaZalazar@users.noreply.github.com>
2021-10-04 14:17:30 -0300

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { HeroContainer } from './Styles';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button'
+import { useDispatch } from 'react-redux';
+import { setFlag } from '../../redux/actions';
 
 const Hero = () => {
+    const dispatch = useDispatch()
+    useEffect(()=> {
+        dispatch(setFlag(false))
+    }, [dispatch])
     return (
         <>
             <HeroContainer>
